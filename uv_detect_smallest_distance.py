2f95cec8ce1aaf48f8445b13469e7b2fe40334c1
SilentSibilance <nemo.waxx@gmail.com>
2022-07-22 20:49:02 -0700

    Refactored uv_detect_smallest_distance.py with functions.

--- uv_detect_smallest_distance.py
+++ uv_detect_smallest_distance.py
@@ -18,58 +18,60 @@ uv_pyshitomasi = cv.imread(input_uv)
 gray = cv.cvtColor(uv_original, cv.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 
-# Harris
-dst = cv.cornerHarris(gray, 2, 3, 0.04)
-#dst = cv.dilate(dst, None)
-uv_harris[dst>0.01*dst.max()] = [0,0,255] #makes corner points red
+def find_corners():
+    #  -- Harris --
+    dst = cv.cornerHarris(gray, 2, 3, 0.04)
+    #dst = cv.dilate(dst, None)
+    uv_harris[dst>0.01*dst.max()] = [0,0,255] #makes corner points red
+    # ----
 
-# Py Shi Tomasi
-corners = cv.goodFeaturesToTrack(gray, 14, 0.01, 10) # 18 is number of corners to track
-corners = np.int0(corners) # don't forget this line. Research this line further.
+    # -- Py Shi Tomasi --
+    corners = cv.goodFeaturesToTrack(gray, 18, 0.01, 10) # 18 is number of corners to track
+    corners = np.int0(corners) # don't forget this line. Research this line further.
 
-for i in corners:
-    x,y = i.ravel()
-    cv.circle(uv_pyshitomasi,(x,y),3,255,-1)
+    for i in corners:
+        x,y = i.ravel()
+        cv.circle(uv_pyshitomasi,(x,y),3,255,-1)
+    # ----
+    return corners
 
-# TODO: Lazy algorithm to get distance between two corner points. Assume all UV faces have same dimensions.
+def corners_as_list():
+    corners_list = corners.tolist() #list of lists. NOTE this goes 3 layers deep, with extraneous layer around coordinate pair.
+    corners_sorted = sorted(corners_list)
+
+# Lazy algorithm to get distance between two corner points. Assume all UV faces have same dimensions.
 # Distance between two points = dimension for UV face.
-# Find distance between any two points.
 # Sort distances in asscending order, to find smallest distance.
 # TODO: represent as a tuple, with point 1, point 2 and distance.
 # use Euclidean distance. (This works because perfect square. May not work for more complex shapes?)
 # Lazy should definitely be my search key word for fixing sloppy code.
-column_index = 1
-corners_list = corners.tolist() #list of lists. NOTE this goes 3 layers deep, with extraneous layer around coordinate pair.
-corners_sorted = sorted(corners_list)
+# PURPOSE: Find distance between all points. (Any two points.)
+def find_distance(corners):
+    distances = []
 
-distances = []
-
-
-dist = np.linalg.norm(corners[3] - corners[7]) # distance between first and second point
-#TODO: distance between points must return integer. This all could likely be a function.
-# EXERCISE FOR FUN: Find distance between all points.
-
-i = 0
-j = 0
-while i < len(corners):
-    while j < len(corners):
-        #distances.append(2)
-        distances.append(np.linalg.norm(corners[i] - corners[j]))
-        j += 1
+    i = 0
     j = 0
-    i += 1
-distances.sort()
-# remove zeros from distances list
-i = 0
-count = 0
-while (distances[i] == 0.0) & (count < 50): #count added to avoid infinite loop
-    distances.remove(0.0)
-    count += 1
+    # create list of all distances
+    while i < len(corners):
+        while j < len(corners):
+            distances.append(np.linalg.norm(corners[i] - corners[j]))  # distance between first and second point
+            j += 1
+        j = 0
+        i += 1
+    distances.sort()
+    # remove zeros from distances list
+    i = 0
+    count = 0
+    while (distances[i] == 0.0) & (count < 50): #count added to avoid infinite loop
+        distances.remove(0.0)
+        count += 1
 
+    # smallest non-zero value in distances is the distance of UV face's edge -- assuming all faces are identical squares, and mesh is a perfect cube.
+    face_length = distances[0]
+    return face_length
 
-
-# smallest non-zero value in distances is the distance of UV face's edge -- assuming all faces are identical squares, and mesh is a perfect cube.
-face_length = distances[0]
+# CALL FUNCTIONS
+face_len = find_distance(find_corners())
 
 # DISPLAY!
 cv.imshow('Original UV', uv_original)
@@ -81,10 +83,10 @@ cv.imshow('Original UV', uv_original)
 cv.imshow('Py Shi Tomasi UV', uv_pyshitomasi)
 plt.imshow(uv_pyshitomasi)
 
-print("Distances: ")
-print(distances)
+#print("Distances: ")
+#print(distances)
 print("UV Face Length: ")
-print(face_length)
+print(face_len)
 #print(corners)
 #print("Corners unsorted list: ", corners_list)
 #print("Corners sorted array:", corners_sorted)
@@ -96,3 +98,4 @@ if cv.waitKey(0) & 0xff == 27:
 
 # TODO: Handle cases where corner dectection is off by 2 pixels.
 # TODO: Handle y coordinate sorting and corner artifact smoothing.
+#TODO: distance between points must return integer. This all could likely be a function.
